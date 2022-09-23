f2fedc42c85a9fe11b4a8aecad60e80807b0a65a
aluminiobolivia <113138231+aluminiobolivia@users.noreply.github.com>
2022-09-23 16:08:15 -0400

    Delete models directory

--- models/res_currency.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class ResCurrency(models.Model):
-    _inherit = 'res.currency'
-
-    moneda_siat_id = fields.Many2one('siat.tipo.moneda', string='Moneda SIAT')
